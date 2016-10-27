d0b783c5b619c38d0177b08a21b6a3d6a1fb3620
Freddy Flores <freddyflores6666@gmail.com>
2016-10-27 16:59:12 -0600

    Added Photo Gallery- project- 4

---
+++ tipuedrop/tipuedrop_content.js
@@ -0,0 +1,18 @@
+var tipuedrop = {"pages": [
+	{"title": "Hay Bales", "thumb": "img/tbn/01.jpg", "text": "I love hay bales. Took this snap on a drive through the countryside past some straw fields.",  "url": "img/bg/01.jpg"},
+     {"title": "Lake", "thumb": "img/tbn/02.jpg", "text": "The lake was so calm today. We had a great view of the snow on the mountains from here.", "url": "img/bg/02.jpg"},
+     {"title": "Canyon", "thumb": "img/tbn/03.jpg", "text": "I hiked to the top of the mountain and got this picture of the canyon and trees below.", "url": "img/bg/03.jpg"},
+     {"title": "Iceberg", "thumb": "img/tbn/04.jpg", "text": "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.", "url": "img/bg/04.jpg"},
+     {"title": "Desert", "thumb": "img/tbn/05.jpg", "text": "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.", "url": "img/bg/05.jpg"},
+
+     {"title": "Fall", "thumb": "img/tbn/06.jpg", "text": "Fall is coming, I love when the leaves on the trees start to change color.", "url": "img/bg/06.jpg"},
+     {"title": "Plantation", "thumb": "img/tbn/07.jpg", "text": "I drove past this plantation yesterday, everything is so green!", "url": "img/bg/07.jpg"},
+     {"title": "Dunes", "thumb": "img/tbn/08.jpg", "text": "My summer vacation to the Oregon Coast. I love the sandy dunes!", "url": "img/bg/08.jpg"},
+     {"title": "Countryside Lane", "thumb": "img/tbn/09.jpg", "text": "We enjoyed a quiet stroll down this countryside lane.", "url": "img/bg/09.jpg"},
+     {"title": "Sunset", "thumb": "img/tbn/10.jpg", "text": "Sunset at the coast! The sky turned a lovely shade of orange.", "url": "img/bg/10.jpg"},
+     {"title": "Cave", "thumb": "img/tbn/11.jpg", "text": "I did a tour of a cave today and the view of the landscape below was breathtaking.", "url": "img/bg/11.jpg"},
+     {"title": "Bluebells", "thumb": "img/tbn/12.jpg", "text": "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.", "url": "img/bg/12.jpg"}
+     
+]};
+     
+
